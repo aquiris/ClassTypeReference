c1f7ca04923f207ce093742adc2c389af9480423
Marcelo Chaves <marcelochaves17@gmail.com>
2021-07-13 18:16:43 -0300

    Update ClassTypeConstraintAttribute

--- Runtime/ClassTypeConstraintAttribute.cs
+++ Runtime/ClassTypeConstraintAttribute.cs
@@ -1,48 +1,24 @@
-﻿// Copyright (c) Rotorz Limited. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root.
-
-using System;
+﻿using System;
 using UnityEngine;
 
-namespace Aquiris.ClassType.Reflection
+namespace Aquiris.ClassTypeReference.Reflection
 {
-    /// <summary>
-    /// Base class for class selection constraints that can be applied when selecting
-    /// a <see cref="ClassTypeReference"/> with the Unity inspector.
-    /// </summary>
     public abstract class ClassTypeConstraintAttribute : PropertyAttribute
     {
-        private EClassGrouping _grouping = EClassGrouping.ByNamespaceFlat;
         private bool _allowAbstract = false;
-
-        /// <summary>
-        /// Gets or sets grouping of selectable classes. Defaults to <see cref="EClassGrouping.ByNamespaceFlat"/>
-        /// unless explicitly specified.
-        /// </summary>
-        public EClassGrouping Grouping
-        {
-            get => _grouping;
-            set => _grouping = value;
-        }
-
-        /// <summary>
-        /// Gets or sets whether abstract classes can be selected from drop-down.
-        /// Defaults to a value of <c>false</c> unless explicitly specified.
-        /// </summary>
         public bool AllowAbstract
         {
             get => _allowAbstract;
             set => _allowAbstract = value;
         }
 
-        /// <summary>
-        /// Determines whether the specified <see cref="Type"/> satisfies filter constraint.
-        /// </summary>
-        /// <param name="type">Type to test.</param>
-        /// <returns>
-        /// A <see cref="bool"/> value indicating if the type specified by <paramref name="type"/>
-        /// satisfies this constraint and should thus be selectable.
-        /// </returns>
+        private EClassGrouping _grouping = EClassGrouping.ByNamespaceFlat;
+        public EClassGrouping Grouping
+        {
+            get => _grouping;
+            set => _grouping = value;
+        }
+
         public virtual bool IsConstraintSatisfied(Type type)
         {
             return AllowAbstract || !type.IsAbstract;
