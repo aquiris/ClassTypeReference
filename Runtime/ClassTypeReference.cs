d7c19e70ad8b548cae8740c200faa9b3fddd6e1c
Marcelo Chaves <marcelochaves17@gmail.com>
2021-07-13 18:16:59 -0300

    Update ClassTypeReference

--- Runtime/ClassTypeReference.cs
+++ Runtime/ClassTypeReference.cs
@@ -1,41 +1,21 @@
-﻿// Copyright (c) Rotorz Limited. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root.
-
-using System;
+﻿using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-namespace Aquiris.ClassType.Reflection
+namespace Aquiris.ClassTypeReference.Reflection
 {
-    /// <summary>
-    /// Reference to a class <see cref="System.Type"/> with support for Unity serialization.
-    /// </summary>
     [Serializable]
     public sealed class ClassTypeReference : ISerializationCallbackReceiver
     {
-        public static string GetClassRef(Type type)
-        {
-            return type != null
-                ? type.FullName + ", " + type.Assembly.GetName().Name
-                : string.Empty;
-        }
-
-        [SerializeField, FormerlySerializedAs("_classRef")]
-        private string _classRef = string.Empty;
-
         private Type _type;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ClassTypeReference"/> class.
-        /// </summary>
+        [SerializeField]
+        private string _classRef = string.Empty;
+
         public ClassTypeReference()
         {
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ClassTypeReference"/> class.
-        /// </summary>
-        /// <param name="assemblyQualifiedClassName">Assembly qualified class name.</param>
         public ClassTypeReference(string assemblyQualifiedClassName)
         {
             Type = !string.IsNullOrEmpty(assemblyQualifiedClassName)
@@ -43,24 +23,18 @@ namespace Aquiris.ClassType.Reflection
                 : null;
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ClassTypeReference"/> class.
-        /// </summary>
-        /// <param name="type">Class type.</param>
-        /// <exception cref="System.ArgumentException">
-        /// If <paramref name="type"/> is not a class type.
-        /// </exception>
         public ClassTypeReference(Type type)
         {
             Type = type;
         }
 
-        /// <summary>
-        /// Gets or sets type of class reference.
-        /// </summary>
-        /// <exception cref="System.ArgumentException">
-        /// If <paramref name="value"/> is not a class type.
-        /// </exception>
+        public static string GetClassRef(Type type)
+        {
+            return type != null
+                ? $"{type.FullName}, {type.Assembly.GetName().Name}"
+                : string.Empty;
+        }
+
         public Type Type
         {
             get => _type;
@@ -68,7 +42,7 @@ namespace Aquiris.ClassType.Reflection
             {
                 if (value != null && !value.IsClass)
                 {
-                    throw new ArgumentException(string.Format("'{0}' is not a class type.", value.FullName), "value");
+                    throw new ArgumentException($"'{value.FullName}' is not a class type.", "value");
                 }
 
                 _type = value;
@@ -83,7 +57,7 @@ namespace Aquiris.ClassType.Reflection
                 _type = System.Type.GetType(_classRef);
                 if (_type == null)
                 {
-                    Debug.LogWarning(string.Format("'{0}' was referenced but class type was not found.", this._classRef));
+                    Debug.LogWarning($"'{_classRef} was referenced but class type was not found.");
                 }
             }
             else
@@ -113,7 +87,9 @@ namespace Aquiris.ClassType.Reflection
 
         public override string ToString()
         {
-            return Type != null ? Type.FullName : "(None)";
+            return Type != null
+                ? Type.FullName
+                : "(None)";
         }
     }
 }
